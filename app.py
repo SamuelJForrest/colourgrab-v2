67b6f981853a801eb622139815747eacc952b646
samfo <samueljamesforrestweb@gmail.com>
2023-12-16 15:12:58 +0000

    Upload area styling WIP

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
 import os
 from flask import Flask
 from colourgrab import app, routes
 
+load_dotenv()
+port = int(os.getenv('PORT'))
+
 if __name__ == '__main__':
     app.run(
-        debug=os.environ.get('DEVELOPMENT'), 
-        host=os.environ.get('PORT')
+        debug=os.getenv('DEVELOPMENT'), 
+        port=port
     )
