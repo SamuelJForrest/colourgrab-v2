a97d13d2f5feb9fab557ce6c3a183a82bbbcd4a3
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-03-29 19:58:29 +0000

    IMP: #20 Updating jest tests WIP

--- tests/frontend/alerts.test.js
+++ tests/frontend/alerts.test.js
@@ -1,17 +1,5 @@
-import '@testing-library/jest-dom';
 import { closeAlerts } from '../../colourgrab/frontend/js/modules/alerts';
 
 describe('closeAlerts function', () => {
-    test('should remove alert from DOM when close button is clicked', () => {
-        const testAlertWrap = document.createElement('div');
-        testAlertWrap.classList.add('alerts-wrap');
-        document.body.appendChild(testAlertWrap);
-
-        expect(document.querySelector('.alerts-wrap')).toBeInTheDocument();
-
-        // closeAlerts();
-
-        // // Assert that the alertsWrap is removed from the DOM
-        // expect(document.querySelector('.alerts-wrap')).toBeNull();
-    });
+    test('should remove alert from DOM when close button is clicked', () => {});
 });
